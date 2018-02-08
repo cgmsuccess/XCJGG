33c0e904fda3b6e30addc179ff0f87c6c0da476b
cgmsuccess <cgmsuccess@163.com>
2018-02-08 18:18:38 +0800

    添加具体显示表情的页面 XC_pageShowemotionView

--- JGGDemo/Tools/XC_Tools/XC_InputView/XC_EmotionListView.m
+++ JGGDemo/Tools/XC_Tools/XC_InputView/XC_EmotionListView.m
@@ -10,12 +10,7 @@
 
 @implementation XC_EmotionListView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+
+
 
 @end
---
+++ JGGDemo/Tools/XC_Tools/XC_InputView/XC_pageShowemotionView.h
@@ -0,0 +1,13 @@
+//
+//  XC_pageShowemotionView.h
+//  JGGDemo
+//
+//  Created by gao bin on 2018/2/8.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XC_pageShowemotionView : UIView
+
+@end
